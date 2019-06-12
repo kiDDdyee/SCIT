94290c98c9266c278417647a6b93e8d376c4c4b0
Vass Szilard <vass.szilard93@gmail.com>
2019-06-12 19:40:01 +0300

    ceva

--- oopConcepts/src/sci/feature1/MainTest.java
+++ oopConcepts/src/sci/feature1/MainTest.java
@@ -3,5 +3,7 @@ package sci.feature1;
 public class MainTest {
     public static void main (String args[]) {
         System.out.println("Primul commit pe repo");
+        //dsvjdnw
+        System.out.println("nimic");
     }
 }
